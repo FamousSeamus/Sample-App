bf277f9588a460896729127c51344f2d9697e290
Sean Musick <sean_musick@yahoo.com>
2012-04-14 19:06:47 +0100

    Added user factory and show page test

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :test do
 	gem 'rspec'
 	gem 'spork'
 	gem 'webrat', '0.7.1'
+	gem 'factory_girl_rails', '1.0'
 end
 
 #group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Sean Musick"
+  user.email                  "sean@atebitebite.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
