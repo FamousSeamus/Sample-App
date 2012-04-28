880e53666f7c16c383518692ec4d2d963bc343b8
Sean Musick <sean_musick@yahoo.com>
2012-04-28 18:04:43 +0100

    showing microposts

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -16,7 +16,6 @@ describe Micropost do
     @user = Factory(:user)
     @attr = { :content => "Lorem ipsum"}
   end
-  
   it "should create a new instance with valid attributes" do
     @user.microposts.create!(@attr)
   end
@@ -25,14 +24,26 @@ describe Micropost do
     before(:each) do
       @micropost = @user.microposts.create(@attr)
     end  
-  it "should hava a user attribute" do
-    @micropost.should respond_to(:user)
-  end
-  it "should have the right associated user" do
-    @micropost.user_id.should == @user.id
-    @micropost.user.should == @user
+    it "should hava a user attribute" do
+      @micropost.should respond_to(:user)
+    end
+    it "should have the right associated user" do
+      @micropost.user_id.should == @user.id
+      @micropost.user.should == @user
+    end
   end
   
+  describe "validations" do
+    it "should have a user id" do
+      Micropost.new(@attr).should_not be_valid
+    end
+    it "should require nonblank content" do
+      @user.microposts.build(:content => "   ").should_not be_valid
+    end
+    it "should reject long content" do
+      @user.microposts.build(:content => "a" * 141).should_not be_valid
+    end
+    
   end
 end
 
