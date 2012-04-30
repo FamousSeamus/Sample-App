7e509bd3111a555e07ed3eba8031df3db9fe32e0
Sean Musick <sean_musick@yahoo.com>
2012-04-30 20:55:13 +0100

    Micropost interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   
   resources :sessions, :only =>[:new, :create, :destroy]
   resources :users
+  resources :microposts, :only => [:create, :destroy]
 
   root :to =>  "pages#home"
   match '/contact', :to => 'pages#contact'
@@ -10,6 +11,7 @@ SampleApp::Application.routes.draw do
   match '/signup',    :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
   match '/signout',  :to => 'sessions#destroy'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
