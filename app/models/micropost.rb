e860df97a93c5855bfd965ce49adf9ddfabf69d8
Sean Musick <sean_musick@yahoo.com>
2012-05-01 22:02:21 +0100

    user relationships(following/followers

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -17,3 +17,14 @@ class Micropost < ActiveRecord::Base
   validates :user_id, :presence =>true 
   default_scope :order => 'microposts.created_at DESC'
 end
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
